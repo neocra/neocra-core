c5369e83cdf819c238cc06fe0731b568f65cd97b
Aurélien GALTIER <1593976+swoog@users.noreply.github.com>
2021-08-22 20:15:52 +0200

    Add tests

---
+++ Neocra.Core.Tests/HealthChecksTests.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Neocra.Core.AspNetCore.HealthChecks;
+using Xunit;
+
+namespace Neocra.Core.Tests
+{
+    public class HealthChecksTests
+    {
+        [Fact]
+        public async Task Should_get_version_When_generate_response()
+        {
+            var defaultHttpContext = new DefaultHttpContext();
+            await HealthCheckExtensions.WriteHealthCheckUiResponse(defaultHttpContext,
+                new HealthReport(new Dictionary<string, HealthReportEntry>(), HealthStatus.Healthy, TimeSpan.Zero));
+            
+            Assert.True(defaultHttpContext.Response.Headers.ContainsKey("version"));
+        }
+    }
+}
