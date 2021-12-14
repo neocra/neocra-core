84d1b2388927b0904b02f6a7dd9bccceb050c696
Aurélien GALTIER <1593976+swoog@users.noreply.github.com>
2021-12-14 15:41:19 +0100

    Add logger xunit for write log in tests output

---
+++ Neocra.Core.Logger.Xunit/XUnitLoggerExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Neocra.Core.Logger.Xunit;
+
+public static class XUnitLoggerExtensions
+{
+    public static ILoggingBuilder AddXUnitLogger(
+        this ILoggingBuilder builder, 
+        ITestOutputHelper testOutputHelper)
+    {
+        builder.AddProvider(new XUnitLoggerProvider(testOutputHelper))
+            .AddFilter<XUnitLoggerProvider>(null, LogLevel.Trace);
+        return builder;
+    }
+}
